d05bfeaf7deff5fea33661cd9b280b5f611e599c
queenfiona <fionaityang@gmail.com>
2019-11-22 16:48:49 +0300

    Create PUT and DELETE /api/recipes/:id endpoints

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const Recipe = require('./models/recipe');
 const app = express();
 
 //Connect to MongoDB
-mongoose.connect( 'mongodb+srv://fiona:9VuBTyF4U3G3fu7E@cluster0-xwrdv.mongodb.net/test?retryWrites=true&w=majority',{ useNewUrlParser: true }).then(()=>{
+mongoose.connect( 'mongodb+srv://fiona:9VuBTyF4U3G3fu7E@cluster0-xwrdv.mongodb.net/test?retryWrites=true&w=majority').then(()=>{
 	console.log("Successfully connected to MongoDB Atlas!")
 }).catch((error)=>{
 	console.log("Unable to connect to MongoDB Atlas!");
@@ -44,6 +44,51 @@ app.post('/api/recipes',(req, res, next)=>{
 		});
 	});
 });
+//Update a recipe
+app.put('/api/recipes/:id',(req, res, next)=>{
+	const recipe = new Recipe({
+		_id: req.params.id,
+		title: req.body.title,
+		ingredients: req.body.ingredients,
+		instructions: req.body.instructions,
+		difficulty: req.body.difficulty,
+		time: req.body.time
+	});
+
+	Recipe.updateOne({_id: req.params.id},recipe).then(()=>{
+		res.status(200).json({
+			message: "Recipe updated successfully"
+		});
+	}).catch((error)=>{
+		res.status(400).json({
+			error: error
+		});
+	});
+});
+
+//Delete endpoint
+app.delete('/api/recipes/:id',(req, res, next)=>{
+	Recipe.deleteOne({_id: req.params.id}).then(()=>{
+		res.status(200).json({
+			message: "Deleted"
+		});
+	}).catch((error)=>{
+		res.status(400).json({
+			error: error
+		});
+	});
+});
+
+// Get a recipe with the provided ID from the database
+app.use('/api/recipes/:id', (req, res, next)=>{
+	Recipe.findOne({_id: req.params.id}).then((recipe)=>{
+		res.status(200).json(recipe);
+	}).catch((error)=>{
+		res.status(400).json({
+			error: error
+		});
+	});
+});
 
 //Get all the recipes
 app.use('/api/recipes',(req, res, next)=>{
@@ -56,16 +101,6 @@ app.use('/api/recipes',(req, res, next)=>{
 	});
 });
 
-// Get a recipe with the provided ID from the database
-app.use('/api/recipes/:id', (req, res, next)=>{
-	Recipe.findOne({_id: req.params.id}).then((recipe)=>{
-		console.log(recipe);
-		res.status(200).json(recipe);
-	}).catch((error)=>{
-		res.status(400).json({
-			error: error
-		});
-	});
-});
+
 
 module.exports = app;

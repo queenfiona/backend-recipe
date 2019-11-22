fab2366aa0d416131da673395f2c2ef074b15c02
queenfiona <fionaityang@gmail.com>
2019-11-22 15:40:14 +0300

    Assign type to difficulty

--- models/recipe.js
+++ models/recipe.js
@@ -4,7 +4,7 @@ const recipeSchema = mongoose.Schema({
     title: {type: String, required: true},
     ingredients: {type: String, required: true},
     instructions: {type: String, required: true},
-    difficulty: {type: String, required: true},
+    difficulty: {type: Number, required: true},
     time: {type: Number, required: true}
 });
 
